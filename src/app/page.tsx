08b5e784e79762783c5693cc2d34f45068d2728b
Julio Rubén Sanic Martínez <julio.sanic.gt.256@gmail.com>
2024-06-19 07:44:24 -0600

    metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,15 @@ import Productive from "@/sections/Productive";
 import Testimonials from "@/sections/Testimonials";
 import NewsLetter from "@/sections/NewsLetter";
 import Footer from "@/sections/Footer";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Electroser",
+  description:
+    "Servicios de electricidad en general, instalaciones, reparaciones, mantenimiento, etc.",
+  keywords:
+    "electricidad, electricista, instalaciones, reparaciones, mantenimiento, electrios, electroser",
+};
 
 export default function Home() {
   return (
